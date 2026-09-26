5d1b349bff1474b023615eb77c1085b6abf69117
EJCrispy <ej@soundstasty.com>
2026-09-26 00:37:36 -0700

    test
    test

--- PvPAdventure.cs
+++ PvPAdventure.cs
@@ -51,10 +51,6 @@ public class PvPAdventure : Mod
                 Common.Game.StatTrackers.MatchStatsNetHandler.HandleSnapshotPacket(reader);
                 break;
 
-            case AdventurePacketIdentifier.Hellhex:
-                Common.Combat.EJ.HellhexNetHandler.HandlePacket(reader, whoAmI);
-                break;
-
             case AdventurePacketIdentifier.ShakingChest:
                 Common.NPCs.ShakingChestNetHandler.HandlePacket(reader);
                 break;
